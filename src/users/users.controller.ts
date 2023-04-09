04c64001c3a688ae6309b213a5c3ffc71ab5bdf1
Jung Su <wjdwjdtn92@naver.com>
2023-04-09 12:51:34 +0900

    ✨ users crud 구현 - #3

---
+++ src/users/users.controller.ts
@@ -0,0 +1,63 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { UserEntity } from './entities/user.entity';
+import { ApiBearerAuth, ApiCreatedResponse, ApiTags } from '@nestjs/swagger';
+import { Public } from 'src/auth/decorators/public.decorator';
+
+@Controller('users')
+@ApiTags('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  @Public()
+  @ApiCreatedResponse({ type: UserEntity })
+  async create(@Body() createUserDto: CreateUserDto) {
+    return new UserEntity(await this.usersService.create(createUserDto));
+  }
+
+  @Get()
+  @ApiBearerAuth()
+  @ApiCreatedResponse({ type: UserEntity, isArray: true })
+  async findAll() {
+    const users = await this.usersService.findAll();
+
+    return users.map((user) => new UserEntity(user));
+  }
+
+  @Get(':username')
+  @ApiBearerAuth()
+  @ApiCreatedResponse({ type: UserEntity })
+  async findOne(@Param('username') username: string) {
+    return new UserEntity(await this.usersService.findOne(username));
+  }
+
+  @Patch(':username')
+  @ApiBearerAuth()
+  @ApiCreatedResponse({ type: UserEntity })
+  async update(
+    @Param('username') username: string,
+    @Body() updateUserDto: UpdateUserDto,
+  ) {
+    return new UserEntity(
+      await this.usersService.update(username, updateUserDto),
+    );
+  }
+
+  @Delete(':username')
+  @ApiBearerAuth()
+  @ApiCreatedResponse({ type: UserEntity })
+  async remove(@Param('username') username: string) {
+    return new UserEntity(await this.usersService.remove(username));
+  }
+}

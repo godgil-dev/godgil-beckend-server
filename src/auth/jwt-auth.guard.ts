a12b001964fa22a39cd9f4bb797c7ad4f402fdb8
Jung Su <wjdwjdtn92@naver.com>
2023-04-09 14:46:28 +0900

    ✨ jwt guard 추가 - #5

---
+++ src/auth/jwt-auth.guard.ts
@@ -0,0 +1,23 @@
+import { Injectable, ExecutionContext } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { AuthGuard } from '@nestjs/passport';
+import { IS_PUBLIC_KEY } from './decorators/public.decorator';
+
+@Injectable()
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  constructor(private reflector: Reflector) {
+    super();
+  }
+
+  canActivate(context: ExecutionContext) {
+    // reflector 으로 IS_PUBLIC_KEY 의 메타데이터 값을 가져옴
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    if (isPublic) {
+      return true;
+    }
+    return super.canActivate(context);
+  }
+}

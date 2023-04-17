e9c0b41105c2610acddd2b079e3d6892290f75b7
Jung Su <wjdwjdtn92@naver.com>
2023-04-17 22:53:51 +0900

    🚚 폴더 위치 수정 - #17

--- src/comments-dislike/comments-dislike.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CommentsDislikeService } from './comments-dislike.service';
-
-@Module({
-  providers: [CommentsDislikeService],
-})
-export class CommentsDislikeModule {}
--- src/comments-like/comments-like.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CommentsLikeService } from './comments-like.service';
-
-@Module({
-  providers: [CommentsLikeService],
-})
-export class CommentsLikeModule {}

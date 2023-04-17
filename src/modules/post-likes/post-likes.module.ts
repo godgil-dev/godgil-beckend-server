c5fbf29286a4be6283910aeeb73cb50c3ec901ac
Jung Su <wjdwjdtn92@naver.com>
2023-04-17 23:55:59 +0900

    ✨ 게시글 좋아요 생성, 삭제 기능 구현 - #22

---
+++ src/modules/post-likes/post-likes.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { PostLikesService } from './post-likes.service';
+import { PostLikesController } from './post-likes.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
+
+@Module({
+  controllers: [PostLikesController],
+  providers: [PostLikesService],
+  imports: [PrismaModule],
+})
+export class PostLikesModule {}

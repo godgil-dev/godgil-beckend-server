7546a2e4d3657e60984bbebc527f329ef340c44a
Jung Su <wjdwjdtn92@naver.com>
2023-04-24 15:50:24 +0900

    🔧 pm2 설정 파일 추가 - #46

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  apps: [
+    {
+      name: 'my-app',
+      script: 'dist/src/main.js',
+      instances: 'max',
+      exec_mode: 'cluster',
+      // error_file: '/app/logs/error.log',
+      // out_file: '/app/logs/out.log',
+      // log_date_format: 'YYYY-MM-DD HH:mm:ss',
+      wait_ready: true,
+      listen_timeout: 50000,
+      kill_timeout: 5000,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+    },
+  ],
+};

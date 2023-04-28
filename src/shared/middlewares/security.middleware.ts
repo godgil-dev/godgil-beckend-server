77aaa02a009b8df5ca31aa31fecfd7035ffb660d
Jung Su <wjdwjdtn92@naver.com>
2023-04-28 15:55:16 +0900

    ✨ SecurityMiddleware 추가 - #60

---
+++ src/shared/middlewares/security.middleware.ts
@@ -0,0 +1,21 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { NextFunction, Request, Response } from 'express';
+import helmet from 'helmet';
+
+@Injectable()
+export class SecurityMiddleware implements NestMiddleware {
+  use(req: Request, res: Response, next: NextFunction) {
+    helmet({
+      contentSecurityPolicy: {
+        directives: {
+          defaultSrc: ["'self'"],
+          scriptSrc: ["'self'", "'unsafe-inline'", "'unsafe-eval'"],
+          styleSrc: ["'self'", 'https:', "'unsafe-inline'"],
+          baseUri: ["'self'"],
+          fontSrc: ["'self'", 'https:', 'data:'],
+        },
+      },
+      // hidePoweredBy: true,
+    })(req, res, next);
+  }
+}

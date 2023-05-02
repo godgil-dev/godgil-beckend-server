ccd26328581861552ddbde3a512de77fd53dfec7
Jung Su <wjdwjdtn92@naver.com>
2023-05-02 11:54:42 +0900

    🔨 user avatar 삭제 기능 추가  - #65

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -3,6 +3,8 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 import * as bcrypt from 'bcrypt';
+import * as fs from 'fs';
+import * as path from 'path';
 
 export const roundsOfHashing = 10;
 
@@ -56,7 +58,51 @@ export class UsersService {
     });
   }
 
-  remove(id: number) {
+  async remove(id: number) {
+    const user = await this.findOneById(id);
+
+    if (user.avatarUrl) {
+      const filePath = path.join(
+        __dirname,
+        'uploads',
+        '..',
+        '..',
+        user.avatarUrl,
+      );
+      fs.unlinkSync(filePath);
+    }
     return this.prisma.user.delete({ where: { id } });
+  }
+
+  async uploadAvatar(userId: number, fileName: string) {
+    const avatarUrl = `uploads/${fileName}`;
+    const user = await this.prisma.user.update({
+      where: { id: userId },
+      data: { avatarUrl },
+    });
+    return user;
+  }
+
+  async removeAvatar(userId: number) {
+    const user = await this.findOneById(userId);
+    console.log(user);
+
+    if (user.avatarUrl) {
+      const filePath = path.join(
+        __dirname,
+        'uploads',
+        '..',
+        '..',
+        user.avatarUrl,
+      );
+
+      fs.unlinkSync(filePath);
+      return await this.prisma.user.update({
+        where: { id: userId },
+        data: { avatarUrl: null },
+      });
+    }
+
+    return user;
   }
 }

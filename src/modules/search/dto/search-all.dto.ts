a896ee58c12ebf2220b873d7caf4653ddd76ecd3
Jung Su <wjdwjdtn92@naver.com>
2023-05-11 11:08:38 +0900

    ✨ SearchAllDto 추가 - #78

---
+++ src/modules/search/dto/search-all.dto.ts
@@ -0,0 +1,15 @@
+import { IsIn, IsNumberString, IsOptional, IsString } from 'class-validator';
+import { PaginationQueryDto } from 'src/shared/dto/pagenation-query.dto';
+
+export class SearchAllDto extends PaginationQueryDto {
+  @IsString()
+  @IsOptional()
+  query?: string;
+
+  @IsIn(['all', 'book', 'proCon'])
+  type: string;
+
+  @IsNumberString()
+  @IsOptional()
+  isbn?: string;
+}

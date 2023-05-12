f761ff6adb823e07b7a0b2a38900d0033e60a4ed
Jung Su <wjdwjdtn92@naver.com>
2023-05-12 13:25:16 +0900

    ✨ NotificationService 추가 - #83

---
+++ src/modules/notification/notification.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@nestjs/common';
+import { CreateNotificationDto } from './dto/create-notification.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class NotificationService {
+  constructor(private prisma: PrismaService) {}
+
+  create(createNotificationDto: CreateNotificationDto) {
+    return this.prisma.commentNotification.create({
+      data: createNotificationDto,
+    });
+  }
+
+  async getNotificationsForUser(userId: number) {
+    const commentNotifications = this.prisma.commentNotification.findMany({
+      where: { userId, readStatus: false },
+      include: {
+        Comment: true,
+      },
+    });
+
+    return (await commentNotifications).map((notification) => ({
+      id: notification.id,
+      postId: notification.Comment.postId,
+      commentId: notification.commentId,
+      content: notification.Comment.content,
+      createdAt: notification.Comment.createdAt,
+      updatedAt: notification.Comment.updatedAt,
+      readStatus: notification.readStatus,
+    }));
+  }
+
+  async markAsRead(notificationId: number) {
+    const notification = await this.prisma.commentNotification.update({
+      where: { id: notificationId },
+      data: { readStatus: true },
+      include: { Comment: true },
+    });
+
+    return {
+      id: notification.id,
+      postId: notification.Comment.postId,
+      commentId: notification.commentId,
+      content: notification.Comment.content,
+      createdAt: notification.Comment.createdAt,
+      updatedAt: notification.Comment.updatedAt,
+      readStatus: notification.readStatus,
+    };
+  }
+}

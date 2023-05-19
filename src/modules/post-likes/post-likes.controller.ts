81b564e80e82cc2fb9130e252c6ca251c63a3fee
Jung Su <wjdwjdtn92@naver.com>
2023-05-19 11:36:27 +0900

    ✨ /post-likes/me 엔드포인트에 findAll 추가 - #89

--- src/modules/post-likes/post-likes.controller.ts
+++ src/modules/post-likes/post-likes.controller.ts
@@ -6,19 +6,22 @@ import {
   ParseIntPipe,
   Req,
   UseGuards,
+  Get,
+  Query,
 } from '@nestjs/common';
 import { PostLikesService } from './post-likes.service';
-import { ApiTags, ApiBearerAuth } from '@nestjs/swagger';
+import { ApiTags, ApiBearerAuth, ApiQuery } from '@nestjs/swagger';
 import UserRequest from '../auth/types/user-request.interface';
 import { ParamPostExistGuard } from '../posts/guards/param-post-exits.guard';
+import { PaginationQueryDto } from 'src/shared/dto/pagenation-query.dto';
 
 @ApiTags('likes')
-@Controller('likes/:postId')
+@Controller('likes')
 export class PostLikesController {
   constructor(private readonly postLikesService: PostLikesService) {}
 
   @ApiBearerAuth()
-  @Post()
+  @Post(':postId')
   @UseGuards(ParamPostExistGuard)
   async create(
     @Param('postId', ParseIntPipe) postId: number,
@@ -28,12 +31,43 @@ export class PostLikesController {
   }
 
   @ApiBearerAuth()
-  @Delete()
+  @Delete(':postId')
   @UseGuards(ParamPostExistGuard)
   async remove(
     @Param('postId', ParseIntPipe) postId: number,
     @Req() request: UserRequest,
   ) {
     return await this.postLikesService.remove(postId, request.user.id);
+  }
+
+  @ApiBearerAuth()
+  @ApiQuery({ name: 'limit', type: Number, required: true })
+  @ApiQuery({ name: 'page', type: Number, required: true })
+  @ApiQuery({ name: 'sortBy', type: String, required: false })
+  @Get('/me')
+  async findAll(
+    @Query() paginationQueryDto: PaginationQueryDto,
+    @Req() request: UserRequest,
+    @Query() sortBy: 'lastest' | 'popular' = 'lastest',
+  ) {
+    const { page, limit } = paginationQueryDto;
+    const offset = (page - 1) * limit;
+
+    const { posts, totalCount } = await this.postLikesService.findAllByUserId(
+      request.user?.id || -1,
+      limit,
+      offset,
+      sortBy,
+    );
+
+    return {
+      posts,
+      pageInfo: {
+        page,
+        totalCount,
+        currentCount: posts.length,
+        totalPage: Math.ceil(totalCount / limit),
+      },
+    };
   }
 }

a09c7c6c210455a12846216b7610b00c8d695f34
Jung Su <wjdwjdtn92@naver.com>
2023-06-05 17:58:15 +0900

    🐛 유효성 검삭 username에서 email로 수정 - #131

--- src/modules/auth/strategys/jwt.strategy.ts
+++ src/modules/auth/strategys/jwt.strategy.ts
@@ -15,7 +15,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(payload: any) {
-    const user = await this.usersService.findOne(payload.username);
+    const user = await this.usersService.findOneByEmail(payload.email);
     return user;
   }
 }
